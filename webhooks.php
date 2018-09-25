5abb26599f8b68df29623f0132d87324bf266a39
tbot888 <43582885+tbot888@users.noreply.github.com>
2018-09-26 01:23:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,11 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text,
+				//'imageThumbnail' => "",
+            			//'imageFullsize' => '',
+				'stickerPackageId' => 1,
+            			'stickerId' => 410
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
