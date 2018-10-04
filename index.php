4a8c8ba68f83a6ece8dcbf91019cd8d17d8d9126
cbot888 <43582885+cbot888@users.noreply.github.com>
2018-10-04 15:37:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 include_once('simple_html_dom.php');
 $url = 'https://www.jetsadabet.com/login';
  
-$html = file_get_html($url);
+$html = file_get_html($url).'ok';
 echo $html;
 echo "Hello LINE BOT...";
