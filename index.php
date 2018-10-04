1450c693cc12e5c9bb53497c70d933cfa7a05b84
cbot888 <43582885+cbot888@users.noreply.github.com>
2018-10-04 15:26:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
+$url = 'https://www.jetsadabet.com/login';
+ 
+$html = file_get_html($url);
+echo $html;
+$links = array();
+foreach($html->find('a[class="postlink"]') as $a) {
+ $links[] = $a->href;
+}
+ 
+print_r($links);
+echo $links;
 echo "Hello LINE BOT";
