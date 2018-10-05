e4f9b28006586942408358a646df48d691f3c33e
cbot888 <43582885+cbot888@users.noreply.github.com>
2018-10-05 14:12:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			if($event['message'] == 'ok')
+			if($message == 'ok')
 				$text = $event['source']['groupId'];
 			else
 				$text = '888';
